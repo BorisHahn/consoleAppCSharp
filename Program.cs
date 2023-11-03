f2eff69a96ecb68f17d990d0b15acf88e6f20d51
Boris Gaidukevich <Borishahn@yandex.ru>
2023-11-03 21:46:45 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine(x);
+
+            int y = 20;
+            Console.WriteLine(y);
+        }
+    }
+}
